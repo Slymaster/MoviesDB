0ff24d5ba458a061174117b891f1948667286caa
sylvainrey <sylvain.rey.pro@gmail.com>
2019-07-04 18:03:51 +0200

    first com

---
+++ DataModel.edmx.sql
@@ -0,0 +1,84 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 07/01/2019 14:49:37
+-- Generated from EDMX file: C:\Users\s.rey\source\repos\MoviesDB\MoviesDB\DataModel.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [Database];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'UserSet'
+CREATE TABLE [dbo].[UserSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Login] nvarchar(max)  NOT NULL,
+    [Password] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'MovieSet'
+CREATE TABLE [dbo].[MovieSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL,
+    [Description] nvarchar(max)  NOT NULL,
+    [User_Id] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'UserSet'
+ALTER TABLE [dbo].[UserSet]
+ADD CONSTRAINT [PK_UserSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'MovieSet'
+ALTER TABLE [dbo].[MovieSet]
+ADD CONSTRAINT [PK_MovieSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [User_Id] in table 'MovieSet'
+ALTER TABLE [dbo].[MovieSet]
+ADD CONSTRAINT [FK_Entity1Entity2]
+    FOREIGN KEY ([User_Id])
+    REFERENCES [dbo].[UserSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_Entity1Entity2'
+CREATE INDEX [IX_FK_Entity1Entity2]
+ON [dbo].[MovieSet]
+    ([User_Id]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataModelContainer ctx = new DataModelContainer();
+
+            User user = new User();
+            user.Login = "bibi";
+            user.Password = "test";
+
+            //var query = ctx.UserSet.Where(u => u.Password.Contains("t")).Select(u => u.Login).ToList();
+            
+            ctx.UserSet.Add(user);
+
+            ctx.SaveChanges();
+
+        }
+    }
+}
